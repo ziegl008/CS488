9b8637e8f637c959133ae5a7245c03bb39161d1a
Erik Ziegler <ziegl008@csusm.edu>
2023-08-06 15:39:53 -0700

    final project files

---
+++ FinalProject.ino
@@ -0,0 +1,85 @@
+#include <Arduino_APDS9960.h>
+#include <Arduino_LPS22HB.h>
+
+/*
+  User defined variables
+*/
+float tempThreshold = 50;         // Minimum temperature (f) to send alerts
+int minSecsFromFirstAlert = 15;  // 5 min
+int minSecsBetweenEmails = 10;    // 1 min
+
+// true - receive alerts when above tempThreshold
+// false - do not factor temperature into alerts
+bool alertOnTemp = true;
+
+// true - receive alerts when second motion has not been detected within minSecsFromFirstAlert
+// false - do not factor second motion into alerts
+bool alertOnSecondMotion = true;
+
+/*
+  Program variables
+*/
+long lastMotion = -minSecsFromFirstAlert * 1000; // Initiliaze to large negative value
+long lastSend = -minSecsBetweenEmails * 1000; // Initiliaze to large negative value
+bool firstMotionDetected = false; // Flip counter used for second motion alert
+
+void setup() {
+  Serial.begin(9600); // Initiliaze serial connection
+  while (!Serial)
+    ;
+  if (!BARO.begin()) {
+    Serial.println("Failed to initialize pressure sensor!");
+      ;
+  }
+  if (!APDS.begin()) {
+    Serial.println("Error initializing APDS9960 sensor!");
+  }
+}
+
+void loop() {
+  unsigned long now = millis(); // Initiliaze time passed
+  float pressure = BARO.readPressure(); // Read pressure. Necessary for deriving temperature.
+  float temperature_C = BARO.readTemperature(); // Get temperature in Celsius
+  float temperature_F = ((temperature_C * 1.8) + 32); //  Convert to Fahrenheit
+
+  if (APDS.proximityAvailable()) { // If proximity is working
+    // read the proximity
+    // - 0   => close
+    // - 255 => far
+    // - -1  => error
+    int proximity = APDS.readProximity();
+
+    if (proximity >= 0 && proximity <= 230) { // If motion detected
+      if (now > (lastSend + minSecsBetweenEmails * 1000)) { // If more than minSecsBetweenEmails has passed since last 
+                                                            // motion alert
+        if (alertOnTemp && temperature_F >= tempThreshold) { // If temperature alerts are on and ambient temperature 
+                                                             // is above threshold
+          Serial.print("TEMPERATURE MOVEMENT");
+          Serial.print(",");
+          Serial.print(tempThreshold);
+          Serial.print(",");
+          Serial.println(temperature_F);
+        } 
+        else { // Motion detected but temperature not evaluated
+          Serial.println("MOVEMENT");
+        }
+        lastSend = now; // Update lastSend time for cooldown between alerts
+      } 
+      else { // Motion detected but cooldown has not elapsed
+        Serial.println("TOO SOON");
+      }
+      lastMotion = now; // Update lastMotion time for cooldown between alerts
+      firstMotionDetected = !firstMotionDetected; // Flip flag. First motion will be true and second motion back to false.
+    } 
+    // If second motion alert is on, motion is first time, and more tha nminSecsFromFirstAlert has passed since last motion
+    else if (alertOnSecondMotion && firstMotionDetected && (now > (lastMotion + minSecsFromFirstAlert * 1000))) { 
+      Serial.println("SECOND MOTION MISSED");
+      // Reset lastMotion so alert pings every minSecsFromFirstAlert until second motion has been detected
+      lastMotion = now;
+    } 
+    else { // Nothing is happening
+      Serial.println("QUIET");
+    }
+  }
+  delay(150);
+}
---
+++ petalert.py
@@ -0,0 +1,73 @@
+import time
+import serial
+import smtplib
+
+TO = '[RECIPIENT EMAIIL HERE]'
+GMAIL_USER = '[SENDER EMAIL HERE]'
+GMAIL_PASS = '[SENDER EMAIL APP PASSWORD HERE]'
+
+SUBJECT_DEFAULT = 'First Motion Detected'
+BODYTEXT_DEFAULT = 'Motion has been detected at the dog door.'
+SUBJECT_TEMP = 'Temperature Alert First Motion Detected'
+BODYTEXT_TEMP = 'Motion has been detected at the dog door. Ambient temperature is above threshold.'
+SUBJECT_SECONDMOTION = 'Second Motion Missed'
+BODYTEXT_SECONDMOTION = 'Motion has not been detected at your dog door since first motion. Please check on your dog.'
+  
+ser = serial.Serial('COM3', 9600)
+
+def send_email_default():
+    print("Sending Email")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + SUBJECT_DEFAULT + '\n'
+    print(header)
+    msg = header + '\n' + BODYTEXT_DEFAULT + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, TO, msg)
+    smtpserver.close()
+    
+def send_email_temp_alert(temp1, temp2):
+    print("Sending Temp Email")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + SUBJECT_TEMP + '\n'
+    print(header)
+    msg = header + '\n' + BODYTEXT_TEMP + '\n'
+    msg = msg + '\n' + 'Temperature Threshold: ' + temp1 + ' F' + '\n'
+    msg = msg + '\n' + 'Ambient Temperature: ' + temp2 + ' F' + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, TO, msg)
+    smtpserver.close()
+    
+def send_email_second_motion():
+    print("Sending Second Motion Email")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + SUBJECT_SECONDMOTION + '\n'
+    print(header)
+    msg = header + '\n' + BODYTEXT_SECONDMOTION + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, TO, msg)
+    smtpserver.close()
+    
+while True:
+    message = ser.readline()
+    message = message.decode('utf-8')
+    print(message)
+    if message[0:4] == "MOVE":
+        send_email_default()
+    if message[0:4] == "TEMP":
+        splitMessage = message.split(",")
+        tempThreshold = splitMessage[1]
+        ambientTemp = splitMessage[2]
+        ambientTemp = ambientTemp[0:-2]
+        send_email_temp_alert(tempThreshold, ambientTemp)
+    if message[0:4] == "SECO":
+        send_email_second_motion()
+    time.sleep(0.5)
